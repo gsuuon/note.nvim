d28f1a180e8357835258e9be8061b1e378fb03d9
Steven Sun <gsuuon@gmail.com>
2023-06-25 12:20:40 -0500

    test: add children tests

--- test/items.lua
+++ test/items.lua
@@ -56,22 +56,23 @@ local expected_items =
       }
     } }
 
+---Utility function to help set test expected values. Sets the register 'x' to a value
 local function to_reg(x)
   vim.fn.setreg('x', vim.inspect(x))
 end
 
-local function test_item_parsing()
-  local function collect(lines)
-    local xs = {}
+local function collect(iter_fun)
+  local results = {}
 
-    for item in items.items(lines) do
-      table.insert(xs, item)
-    end
-
-    return xs
+  for x in iter_fun do
+    table.insert(results, x)
   end
 
-  local result = collect(test_lines)
+  return results
+end
+
+local function test_item_parsing()
+  local result = collect(items.items(test_lines))
 
   assert(vim.deep_equal(result, expected_items))
 
@@ -98,5 +99,55 @@ local function test_scan_items()
   return result
 end
 
+local function test_children()
+  local lines = {
+    'a',
+    '- b',
+    ' * c',
+    ' [ c1',
+    '   * c2',
+    '   feafe',
+    '- d',
+    'e'
+  }
+
+  local expected_children =
+    { {
+      body = "c",
+      marker = "*",
+      position = {
+        col = 1,
+        row = 2
+      }
+    }, {
+      body = "c1",
+      marker = "[",
+      position = {
+        col = 1,
+        row = 3
+      }
+    }, {
+      body = "c2",
+      marker = "*",
+      position = {
+        col = 3,
+        row = 4
+      }
+    } }
+
+  local first_b = items.find_item_matching_iter({
+    marker = '-',
+    body = '.'
+  }, table.pack(ipairs(lines)))
+
+
+  local children = collect(items.children(first_b, lines))
+
+  assert(vim.deep_equal(children, expected_children))
+end
+
 test_item_parsing()
 test_scan_items()
+test_children()
+
+vim.notify('All tests passed')
