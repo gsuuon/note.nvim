f98d4c8a0f3c551e6473ea0812ec972fbb979309
Steven Sun <gsuuon@gmail.com>
2024-04-18 20:45:56 -0500

    feat: treesitter

---
+++ queries/note/highlights.scm
@@ -0,0 +1,70 @@
+(item
+  (marker
+    (marker_task
+      ((marker_task_pending) @note.pending.marker)))
+  ((content) @note.pending.content))
+
+(item
+  (marker
+    (marker_task
+      ((marker_task_paused) @note.paused.marker)))
+  ((content) @note.paused.content))
+
+(item
+  (marker
+    (marker_task
+      ((marker_task_done) @note.done.marker)))
+  ((content) @note.done.content))
+
+(item
+  (marker
+    (marker_task
+      ((marker_task_cancelled) @note.cancelled.marker)))
+  ((content) @note.cancelled.content))
+
+(item
+  (marker
+    (marker_task
+      ((marker_task_current) @note.current.marker)))
+  ((content) @note.current.content))
+
+(item
+  (marker
+    (marker_property
+      ((marker_property_info) @note.info.marker)))
+  ((content) @note.info.content))
+
+(item
+  (marker
+    (marker_property
+      ((marker_property_label) @note.label.marker)))
+  ((content) @note.label.content ))
+
+(code_block) @markup.raw.block
+
+(section_header) @markup.heading.1
+
+(section
+  (section_children
+    (section
+      ((section_header) @markup.heading.2))))
+
+
+(section
+  (section_children
+    (section
+      (section_children
+        (section
+          ((section_header) @markup.heading.3))))))
+
+
+(section
+  (section_children
+    (section
+      (section_children
+        (section
+          (section_children
+            (section
+              ((section_header) @markup.heading.4))))))))
+
+
---
+++ queries/note/injections.scm
@@ -0,0 +1,4 @@
+(code_block
+  (code_block_language) @_lang
+  (code_block_content) @injection.content
+  (#set-lang-from-info-string! @_lang))
